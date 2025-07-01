087395f64e567bb052b1bc8da1f869928597a0aa
Yuhua Ke <keyhzgsz@outlook.com>
2025-07-01 08:19:45 +0000

    fix:gaia dataset file attach issue and evaluation script format support

--- evaluation/nb_agent_eval/run_eval.sh
+++ evaluation/nb_agent_eval/run_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python main.py \
-    --datasets bamboogle musique simpleqa hotpotqa \
-    --config_path /root/AgentRL/evaluation/nb_agent_eval/config/sampler_config_direct.yaml
+    --datasets gaia \
+    --config_path /root/l0/evaluation/nb_agent_eval/config/sampler_config_claude.yaml
